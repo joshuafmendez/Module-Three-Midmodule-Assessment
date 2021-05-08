bc4a1510443acfb11e251939f04c5a1b2df62a78
joshuamendez <joshuafmendez@gmail.com>
2021-05-08 15:31:32 -0400

    updated to use Hooks

--- src/App.js
+++ src/App.js
@@ -1,42 +1,74 @@
-import React from "react";
+import React,{useState} from "react";
 import ProductList from "./Components/ProductList";
 import Checkout from "./Components/Checkout";
 import Cart from "./Components/Cart";
 import formatPrice from "./helpers/formatPrice";
 import './App.css';
 
-export default class App extends React.Component {
+const App = () => {
+  const [items,setItems] = useState([]);
 
-  state = { items: [] }
+  let subtotal = 0;
+  const tax = 0.05;
+  let subTax = 1;
+  let total = 0;
+  items.forEach(item => subtotal+=item.price);
+  subTax = subtotal * tax
+  total = subtotal + subTax;
 
-  addProduct = (item) => {
-    this.setState((prevState) => {
-      return {items: [item, ...prevState.items] };
-    })
+  const addProduct = item => {
+    setItems((prevItems) => {
+      return [item, ...prevItems]
+    });
   }
 
-  render() {
-    const {items} = this.state;
-    let subtotal = 0;
-    const tax = 0.05;
-    let subTax = 1;
-    let total = 0;
-    items.forEach(item => subtotal+=item.price);
-    subTax = subtotal * tax
-    total = subtotal + subTax;
+  return (
+    <div className="App">
+      <ProductList addProduct={addProduct}/>
+      <div>
+        <Cart items={items}
+              subtotal={formatPrice(subtotal)}
+              subTax={formatPrice(subTax)}
+              total={formatPrice(total)}/>
+        <Checkout total={formatPrice(total)}/>
+      </div>
+    </div>
+  );
+}
+export default App;
+
+// export default class App extends React.Component {
+
+//   state = { items: [] }
+
+//   addProduct = (item) => {
+//     this.setState((prevState) => {
+//       return {items: [item, ...prevState.items] };
+//     })
+//   }
+
+//   render() {
+//     const {items} = this.state;
+//     let subtotal = 0;
+//     const tax = 0.05;
+//     let subTax = 1;
+//     let total = 0;
+//     items.forEach(item => subtotal+=item.price);
+//     subTax = subtotal * tax
+//     total = subtotal + subTax;
     
 
-    return (
-      <div className="App">
-        <ProductList addProduct={this.addProduct}/>
-        <div>
-          <Cart items={items}
-                subtotal={formatPrice(subtotal)}
-                subTax={formatPrice(subTax)}
-                total={formatPrice(total)}/>
-          <Checkout total={formatPrice(total)}/>
-        </div>
-      </div>
-    );
-  }
-}
+//     return (
+//       <div className="App">
+//         <ProductList addProduct={this.addProduct}/>
+//         <div>
+//           <Cart items={items}
+//                 subtotal={formatPrice(subtotal)}
+//                 subTax={formatPrice(subTax)}
+//                 total={formatPrice(total)}/>
+//           <Checkout total={formatPrice(total)}/>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-export default class Checkout extends React.Component {
-    state = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        creditCard: '',
-        zipCode: '',
-    }
-    handleSubmit = (e) => {
+const Checkout = (props) => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName,setLastName] = useState('');
+    const [email,setEmail] = useState('');
+    const [creditCard,setCreditCard] = useState('');
+    const [zipCode,setZipCode] = useState('');
+
+    const handleSubmit = (e) => {
         e.preventDefault();
-        const {total} = this.props
-        const {firstName, lastName, email, zipCode, creditCard} = this.state;
         if(!firstName || !lastName || !email){
             window.alert('Input is not valid');
         } else if (creditCard.length !== 16){
@@ -22,41 +19,112 @@ export default class Checkout extends React.Component {
             Zip code is not valid`);
         }
         window.alert(`Yay! Purchase complete!
-                    You will be charged ${total}`)
+                    You will be charged ${props.total}`)
     }
-    handleChange = (e) => {
-        const {name, value} = e.target;
-        this.setState({[name]: value})
+    const handleFName = (e) => {
+        setFirstName(e.target.value)
     }
-    render(){
-        const {firstName, lastName, email, zipCode, creditCard} = this.state;
-        return (
-            <section>
-                <h2>Checkout</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <label htmlFor='firstName'>First Name</label>
-                    <p>
-                        <input id='firstName' name='firstName' value={firstName} onChange={this.handleChange}/>
-                    </p>
-                    <label htmlFor='lastName'>Last Name</label>
-                    <p>
-                        <input id='lastName' name='lastName' value={lastName} onChange={this.handleChange}/>
-                    </p>
-                    <label htmlFor='email'>Email</label>
-                    <p>
-                        <input id='email' name='email' value={email} onChange={this.handleChange}/>
-                    </p>
-                    <label htmlFor='creditCard'>Credit Card</label>
-                    <p>
-                        <input id='creditCard' name='creditCard' value={creditCard} onChange={this.handleChange}/>
-                    </p>
-                    <label htmlFor='zipCode'>Zip Code</label>
-                    <p>
-                        <input id='zipCode' name='zipCode' value={zipCode} onChange={this.handleChange}/>
-                    </p>
-                    <button>Buy Now</button>
-                </form>
-            </section>
-        )
+    const handleLName = (e) => {
+        setLastName(e.target.value)
     }
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+    const handleCCard = (e) => {
+        setCreditCard(e.target.value)
+    }
+    const handleZCode = (e) => {
+        setZipCode(e.target.value)
+    }
+    return (
+        <section>
+            <h2>Checkout</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='firstName'>First Name</label>
+                <p>
+                    <input id='firstName' name='firstName' value={firstName} onChange={handleFName}/>
+                </p>
+                <label htmlFor='lastName'>Last Name</label>
+                <p>
+                    <input id='lastName' name='lastName' value={lastName} onChange={handleLName}/>
+                </p>
+                <label htmlFor='email'>Email</label>
+                <p>
+                    <input id='email' name='email' value={email} onChange={handleEmail}/>
+                </p>
+                <label htmlFor='creditCard'>Credit Card</label>
+                <p>
+                    <input id='creditCard' name='creditCard' value={creditCard} onChange={handleCCard}/>
+                </p>
+                <label htmlFor='zipCode'>Zip Code</label>
+                <p>
+                    <input id='zipCode' name='zipCode' value={zipCode} onChange={handleZCode}/>
+                </p>
+                <button>Buy Now</button>
+            </form>
+        </section>
+    )
 }
+
+export default Checkout;
+
+// export default class Checkout extends React.Component {
+//     state = {
+//         firstName: '',
+//         lastName: '',
+//         email: '',
+//         creditCard: '',
+//         zipCode: '',
+//     }
+//     handleSubmit = (e) => {
+//         e.preventDefault();
+//         const {total} = this.props
+//         const {firstName, lastName, email, zipCode, creditCard} = this.state;
+//         if(!firstName || !lastName || !email){
+//             window.alert('Input is not valid');
+//         } else if (creditCard.length !== 16){
+//             window.alert(`Input is invalid
+//             Credit card number is not valid`);
+//         } else if (zipCode.length !== 5){
+//             window.alert(`Input is invalid
+//             Zip code is not valid`);
+//         }
+//         window.alert(`Yay! Purchase complete!
+//                     You will be charged ${total}`)
+//     }
+//     handleChange = (e) => {
+//         const {name, value} = e.target;
+//         this.setState({[name]: value})
+//     }
+//     render(){
+//         const {firstName, lastName, email, zipCode, creditCard} = this.state;
+//         return (
+//             <section>
+//                 <h2>Checkout</h2>
+//                 <form onSubmit={this.handleSubmit}>
+//                     <label htmlFor='firstName'>First Name</label>
+//                     <p>
+//                         <input id='firstName' name='firstName' value={firstName} onChange={this.handleChange}/>
+//                     </p>
+//                     <label htmlFor='lastName'>Last Name</label>
+//                     <p>
+//                         <input id='lastName' name='lastName' value={lastName} onChange={this.handleChange}/>
+//                     </p>
+//                     <label htmlFor='email'>Email</label>
+//                     <p>
+//                         <input id='email' name='email' value={email} onChange={this.handleChange}/>
+//                     </p>
+//                     <label htmlFor='creditCard'>Credit Card</label>
+//                     <p>
+//                         <input id='creditCard' name='creditCard' value={creditCard} onChange={this.handleChange}/>
+//                     </p>
+//                     <label htmlFor='zipCode'>Zip Code</label>
+//                     <p>
+//                         <input id='zipCode' name='zipCode' value={zipCode} onChange={this.handleChange}/>
+//                     </p>
+//                     <button>Buy Now</button>
+//                 </form>
+//             </section>
+//         )
+//     }
+// }
